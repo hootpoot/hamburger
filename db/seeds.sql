c38de7966b30cd9da749a0790d62bf4b610b327e
Katie <47231728+hootpoot@users.noreply.github.com>
2019-04-12 17:31:16 -0500

    making connections and trying to get some functions going

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Tender Chicken Feast');
+INSERT INTO burgers (name) VALUES ('Wild Alaskan Salmon & Shrimp');
+INSERT INTO burgers (name) VALUES ('Roasted Turkey Feast');
